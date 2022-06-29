197f60b08c08bf12c7779ae6c275076d125268df
AlexandreNMota <alexandre.acustica@gmail.com>
2022-06-29 09:10:51 -0300

    core Module Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
-
-
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { RouterModule } from '@angular/router';
-import { Error404Component } from './error404/error404.component';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+
 import { CourseModule } from './cursos/curso.module';
+import { CoreModule } from './core/core.module';
+import { Error404Component } from './core/component/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     Error404Component,
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     RouterModule.forRoot([
-      { path: '', redirectTo: 'courses', pathMatch: 'full' },
-      { path: '**', component: Error404Component },      
+      { path: '', redirectTo: 'courses', pathMatch: 'full' },   
+      {
+        path: '**', component: Error404Component},   
     ]),
     CommonModule,
     HttpClientModule,
     CourseModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
